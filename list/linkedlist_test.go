9b34a12c3d551f08283f60c85a830656a49cb770
basokant <basokanthan@gmail.com>
2023-07-09 20:35:37 -0400

    outline tests for singly linked lists

---
+++ list/linkedlist_test.go
@@ -0,0 +1,19 @@
+package list
+
+import "testing"
+
+func TestLinkedListPrepend(t *testing.T) {
+
+}
+
+func TestLinkedListAppend(t *testing.T) {
+
+}
+
+func TestLinkedListDelete(t *testing.T) {
+
+}
+
+func TestLinkedListSearch(t *testing.T) {
+
+}
