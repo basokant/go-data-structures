7e1c3fd0d8764bfa4ef8c441cb9976d892148f3d
basokant <basokanthan@gmail.com>
2023-07-11 00:58:11 -0400

    finish basic array queue implementation

---
+++ queue/arrayqueue.go
@@ -0,0 +1,70 @@
+package queue
+
+import (
+	"errors"
+	"fmt"
+)
+
+type ArrayQueue[T comparable] struct {
+	slice  []T
+	length int
+}
+
+// Dequeue implements Queue.
+func (queue *ArrayQueue[T]) Dequeue() (T, error) {
+	var top T
+	if queue.Empty() {
+		return top, errors.New("queue empty, cannot pop the top value")
+	}
+
+	top = queue.slice[0]
+	queue.slice = queue.slice[1:]
+	queue.length--
+	return top, nil
+}
+
+// Empty implements Queue.
+func (queue ArrayQueue[T]) Empty() bool {
+	return queue.slice == nil || queue.length == 0
+}
+
+// Enqueue implements Queue.
+func (queue *ArrayQueue[T]) Enqueue(data T) error {
+	if queue.slice == nil {
+		queue.slice = []T{}
+	}
+
+	queue.slice = append(queue.slice, data)
+	queue.length++
+	return nil
+}
+
+// Front implements Queue.
+func (queue ArrayQueue[T]) Front() (T, error) {
+	var top T
+	if queue.Empty() {
+		return top, errors.New("queue empty, cannot pop the top value")
+	}
+
+	top = queue.slice[0]
+	return top, nil
+}
+
+// Search implements Queue.
+func (queue ArrayQueue[T]) Search(data T) error {
+	if queue.Empty() {
+		return errors.New("queue empty, cannot search an empty queue")
+	}
+
+	for _, v := range queue.slice {
+		if v == data {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("data was not found in the queue")
+}
+
+func NewArrayQueue[T comparable]() Queue[T] {
+	return &ArrayQueue[T]{}
+}
