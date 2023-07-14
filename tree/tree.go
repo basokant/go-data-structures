671e3c0383da3005939b6a4420b102e38c874de1
basokant <basokanthan@gmail.com>
2023-07-13 23:30:13 -0400

    add to the Tree interface, add TreeNode interface

--- tree/tree.go
+++ tree/tree.go
@@ -1,7 +1,17 @@
 package tree
 
+type Node[T comparable] interface {
+	Root() (*Node[T], error)
+	Left() (*Node[T], error)
+	Right() (*Node[T], error)
+}
+
 type Tree[T comparable] interface {
 	Insert(data T) error
 	Delete(data T) error
-	Search(data T)
+	Search(data T) (*Node[T], error)
+	Traverse(op func(T)) error
+	Height() (int, error)
+	Level(node *Node[T]) (int, error)
+	Size() int
 }
