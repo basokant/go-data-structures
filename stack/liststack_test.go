a561a2358f42312e52447f981ac72d4f93b1c28c
basokant <basokanthan@gmail.com>
2023-11-05 21:31:48 -0500

    finish ListStack unit test

---
+++ stack/liststack_test.go
@@ -0,0 +1,114 @@
+package stack
+
+import "testing"
+
+func TestListStackEmpty(t *testing.T) {
+	emptyStack := NewArrayStack[int]()
+
+	if !emptyStack.Empty() {
+		t.Error("Stack should be empty, was found populated.")
+	}
+
+	slice := []int{0, 1}
+	populatedStack := NewListStackFromArray[int](slice)
+
+	if populatedStack.Empty() {
+		t.Error("Stack should be populated, was found empty.")
+	}
+}
+
+func TestListStackPeek(t *testing.T) {
+	stack := NewListStack[int]()
+
+	_, err := stack.Peek()
+	if err == nil {
+		t.Error("Peek should error if there are no elements, it did not error.")
+	}
+
+	slice := []int{0, 1, 2}
+	stack = NewListStackFromArray[int](slice)
+
+	top, err := stack.Peek()
+	if err != nil {
+		t.Error("Peek should not error if array is populated.")
+	} else if top != 0 {
+		t.Errorf("got %q, wanted %q", top, 0)
+	}
+}
+
+func TestListStackPop(t *testing.T) {
+	stack := NewListStack[int]()
+
+	_, err := stack.Pop()
+	if err == nil {
+		t.Error("Pop should error if there are no elements, it did not error.")
+	}
+
+	slice := []int{0, 1, 2}
+	stack = NewListStackFromArray[int](slice)
+
+	top, err := stack.Pop()
+	if err != nil {
+		t.Error("Pop should not error if array is populated.")
+	} else if top != 0 {
+		t.Errorf("got %q, wanted %q", top, 0)
+	}
+
+	top, err = stack.Pop()
+	if err != nil {
+		t.Error("Pop should not error if array is populated.")
+	} else if top != 1 {
+		t.Errorf("got %q, wanted %q", top, 1)
+	}
+}
+
+func TestListStackPush(t *testing.T) {
+	stack := NewListStack[int]()
+
+	err := stack.Push(0)
+	if err != nil {
+		t.Error("Push should not error when called on empty stack.")
+	}
+
+	err = stack.Push(1)
+	if err != nil {
+		t.Error("Push encountered an error when it should not have.")
+	}
+
+	top, err := stack.Peek()
+	if err != nil {
+		t.Errorf("Peek encountered an error when it should not have.")
+	} else if top != 1 {
+		t.Errorf("got %q, wanted %q", top, 1)
+	}
+
+	top, err = stack.Pop()
+	if err != nil {
+		t.Errorf("Pop encountered an error when it should not have.")
+	} else if top != 1 {
+		t.Errorf("got %q, wanted %q", top, 1)
+	}
+}
+
+func TestListStackSearch(t *testing.T) {
+	stack := NewListStack[int]()
+
+	index, err := stack.Search(0)
+
+	if err == nil || index >= 0 {
+		t.Error("Empty stack should not be searchable.")
+	}
+
+	slice := []int{0, 1, 2, 3, 4}
+	stack = NewListStackFromArray[int](slice)
+
+	for i, v := range slice {
+		index, err = stack.Search(v)
+
+		if err != nil {
+			t.Errorf("Error occured while searching stack for value %q", v)
+		} else if index != i {
+			t.Errorf("got %q, wanted %q", index, i)
+		}
+	}
+}
